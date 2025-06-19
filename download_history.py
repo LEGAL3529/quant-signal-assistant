27cac351397a43f7fe61767c7cf7927da07bf511
LEGAL3529 <47605491+LEGAL3529@users.noreply.github.com>
2025-06-19 12:14:40 +0500

    Update download_history.py

--- download_history.py
+++ download_history.py
@@ -2,8 +2,8 @@ from binance import Client
 import pandas as pd
 import os
 
-API_KEY = 'QCI76yYs5jbuYqS4geSkEZHhkPJq0QZCOz1pXGIEdIOvfN36RLrkbaaa41OI983T'
-API_SECRET = 'EpMK5GTSHJxMFf42IhzGCpVLgxywR60Lz9lWDu7votvjLVgwinY6c5wydupOnVRV'
+API_KEY = 'QCI76yYs5jbuYqS4geSkEZHhkPJq0QZCOz1pXGIEdIOvfN36Rfffffffffffffff'
+API_SECRET = 'EpMK5GTSHJxMFf42IhzGCpVLgxywR60Lz9lWDu7votvjLVgwiffffffffffffff'
 
 client = Client(API_KEY, API_SECRET)
 
